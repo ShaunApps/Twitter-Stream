b4ec66d5278ac6b3e3736b49f375701bc2b7e78f
ShaunApps <shaun.apps@gmail.com>
2016-03-30 16:50:41 -0400

    query db to get tweets and pass them back

--- models/Tweet.js
+++ models/Tweet.js
@@ -10,3 +10,29 @@ var schema = new mongoose.Schema({
 , date          : Date
 , screename     : String
 });
+
+// Create a static getTweets method to return tweet data from the db
+schema.statics.getTweets = function(page, skip, callback) {
+
+  var tweets = [];
+      start = (page * 10) + (skip * 1);
+
+  // query the db, using skip and limit to achieve page chunks
+  Tweet.find({}, 'twid active author avatar body date screename',{skip: start, limit: 10}).sort({date: 'desc'}).exec(function(err, docs){
+
+    // if everything is ok...
+    if(!err) {
+      tweets = docs; // have tweets
+      tweets.forEach(function(tweet){
+        tweet.active = true; // set them to active
+      });
+    }
+
+    // pass them back to the specified callback
+    callback(tweets);
+
+  });
+};
+
+// Return a tweet model based upon the defined schema
+module.exports = Tweet = mongoose.mode('Tweet', schema);
