81e4df03cdb1aae2858b7bb1e82547408301c0bc
ShaunApps <shaun.apps@gmail.com>
2016-03-30 16:13:43 -0400

    set up routes and server on server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var express = require('express'),
 
 // created express instance and set a port variable
 var app = express();
-var port = process.env.PORT || 808;
+var port = process.env.PORT || 8080;
 
 // set handlebars as the template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -25,3 +25,17 @@ mongoose.connect('mongodb://local-host/twitter-stream');
 
 // create an ntwitter instance
 var twit = new twitter(config.twitter);
+
+// Index route
+app.get('/', routes.index);
+
+// Page route
+app.get('/page/:page/:skip', routes.page);
+
+// Set /public as static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Start server
+var server = http.createServer(app).listen(port, function() {
+  console.log("server listening on port " + port);
+});
