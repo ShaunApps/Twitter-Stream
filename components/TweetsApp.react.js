4095d37636cb8fe547c37e459b4d488a5ca6f941
ShaunApps <shaun.apps@gmail.com>
2016-03-31 11:14:33 -0400

    added a function to add a tweet to timeline

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -1,4 +1,37 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var Tweets = require('./Tweets.react.js');
+var Loader = require('./Loader.react.js');
+var NotificationBar = require('./NotificationBar.react.js');
+
+// Export the TweetsApp component
 module.exports = TweetsApp = React.createClass({
+
+  // Method to add tweet to timeline
+  addTweet: function(tweet){
+
+    // get current application state
+    var updated = this.state.tweets;
+
+    // Increment the unread count
+    var count = this.state.count + 1;
+
+    // Increment the skip count
+    var skip = this.state.skip + 1;
+
+    // Add tweet to the beginning of the tweets array
+    updated.unshift(tweet);
+
+    // Set application state
+    this.setState({tweets: updated, count: count, skip: skip});
+  },
+
+  
+
+
+
+
   // Render the component
 
   render: function(){
@@ -10,6 +43,6 @@ module.exports = TweetsApp = React.createClass({
         <NotificationBar count={this.state.count} onShowNewTweets={this.showNewTweets}/>
       </div>
     )
-    
+
   }
-})
+});
