654b0b6be1858443759850d323dcfc4c220106a8
ShaunApps <shaun.apps@gmail.com>
2016-03-30 23:45:12 -0400

    addded TweetsApp class

---
+++ components/TweetsApp.react.js
@@ -0,0 +1,15 @@
+module.exports = TweetsApp = React.createClass({
+  // Render the component
+
+  render: function(){
+
+    return (
+      <div className="tweets-app">
+        <Tweets tweets={this.state.tweets} />
+        <Loader paging={this.state.paging}/>
+        <NotificationBar count={this.state.count} onShowNewTweets={this.showNewTweets}/>
+      </div>
+    )
+    
+  }
+})
